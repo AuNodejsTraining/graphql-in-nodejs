f0ea10474d13c91479697730d4b49866cb90cb3c
bqliu <bqliu@thoughtworks.com>
2022-06-14 18:09:41 +0800

    chore(simple): simplify code

--- src/resolvers.ts
+++
@@ -1,7 +0,0 @@
-import { books } from "./datasets"
-
-export const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,35 @@
 import { ApolloServer } from 'apollo-server';
-import { ApolloServerPluginLandingPageDisabled, ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
-import { resolvers } from './resolvers';
-import { typeDefs } from './typeDefs';
+import { ApolloServerPluginLandingPageDisabled, ApolloServerPluginLandingPageGraphQLPlayground, gql } from 'apollo-server-core';
+import { DocumentNode } from 'graphql';
 import { isProductionEnv } from './utils';
+
+const books: { title: string; author: string; }[] = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+const typeDefs: DocumentNode = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
 
 const landingPagePlugin = () => isProductionEnv()
   ? ApolloServerPluginLandingPageDisabled()
@@ -17,6 +44,5 @@ const landingPagePlugin = () => isProductionEnv()
 export const server = new ApolloServer({
   typeDefs,
   resolvers,
-  csrfPrevention: true,
   plugins: [landingPagePlugin()]
 });
