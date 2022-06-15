f72d329d4c91e2d44bc5027ad0563986e7300bb9
bqliu <bqliu@thoughtworks.com>
2022-06-15 14:06:07 +0800

    feat(subscription): add connect and disconnect hooks and context for subscription

--- src/server/WebSocketServerBuilder.ts
+++ src/server/WebSocketServerBuilder.ts
@@ -4,6 +4,7 @@ import { useServer } from 'graphql-ws/lib/use/ws'
 import { Disposable } from 'graphql-ws/lib/common'
 import { BaseServerBuilder } from './BaseServerBuilder'
 import { GraphQLSchema } from 'graphql'
+import { Context } from '../types/Context'
 
 export class WebSocketServerBuilder extends BaseServerBuilder {
   async build (httpServer: Server, schema: GraphQLSchema): Promise<Disposable> {
@@ -12,7 +13,28 @@ export class WebSocketServerBuilder extends BaseServerBuilder {
       path: '/graphql'
     })
 
-    const serverCleanup = useServer({ schema }, wsServer)
+    const serverCleanup = useServer(
+      {
+        schema,
+        context: async (ctx, message, args): Promise<Context> => {
+          this._logger.debug(ctx)
+          // message.payload is the graphql query from client
+          this._logger.debug(message)
+          this._logger.debug(args)
+          return { authorization: ctx.connectionParams?.authorization as string }
+        },
+        onConnect: async (ctx): Promise<void> => {
+          // Check authentication every time a client connects.
+          this._logger.debug(ctx.connectionParams)
+          this._logger.info('WebSocet Connected!')
+        },
+        onDisconnect: (ctx, code, reason): void => {
+          this._logger.debug(ctx.connectionParams)
+          this._logger.info(`WebSocet Disconnected! (${code}: ${reason})`)
+        }
+      },
+      wsServer
+    )
     return serverCleanup
   }
 }
